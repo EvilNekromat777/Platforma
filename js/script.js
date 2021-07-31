1f30b96addb033897628f5539a40f2e03d14eaa7
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-07-31 23:28:55 +0300

    slider1 + title + adaptive

--- js/script.js
+++ js/script.js
@@ -16,4 +16,31 @@ $('.header__menu').find('.header__link').click(function (event) {
 
 
 
-	//===========================  Меню - бургер  =========================================
+//=========================== Слайдер  =========================================
+
+
+
+// Инициализируем Swiper
+const swiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	// Количество слайдов для показа
+	slidesPerView: 1,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
