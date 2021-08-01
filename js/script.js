da05e6addca0f65fd9cca9769487ab67fdec544d
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-08-01 19:54:46 +0300

    exclusive + adaptive

--- js/script.js
+++ js/script.js
@@ -21,7 +21,34 @@ $('.header__menu').find('.header__link').click(function (event) {
 
 
 // Инициализируем Swiper
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-container_1', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	// Количество слайдов для показа
+	slidesPerView: 1,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
+
+
+
+// Инициализируем Swiper
+const swiper2 = new Swiper('.swiper-container_2', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: true,
